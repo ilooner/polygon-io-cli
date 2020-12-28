18728c989990c49406b3a3cba3136d560fb88584
Timothy Farkas <timothyfarkas@apache.org>
2020-12-27 21:02:45 -0800

    - Added aggregates to polygon client

--- src/main/java/com/github/ilooner/polygoncli/client/PolygonClient.java
+++ src/main/java/com/github/ilooner/polygoncli/client/PolygonClient.java
@@ -1,13 +1,20 @@
 package com.github.ilooner.polygoncli.client;
 
+import com.github.ilooner.polygoncli.client.model.Aggregate;
+import com.github.ilooner.polygoncli.client.model.StockAggregateJSON;
+import com.github.ilooner.polygoncli.client.model.StockAggregateListJSON;
 import com.github.ilooner.polygoncli.client.model.StockExchangeJSON;
 import com.github.ilooner.polygoncli.config.PolygonConfig;
+import com.github.ilooner.polygoncli.output.Outputter;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -16,6 +23,10 @@ import java.util.concurrent.*;
 
 public class PolygonClient {
     public static final Duration REQUEST_TIMEOUT = Duration.ofMinutes(15L);
+    public static final String AGGREGATES_SORT_ASC = "asc";
+    public static final String AGGREGATES_TIMESPAN = "minute";
+    public static final int AGGREGATES_LIMIT = 50000;
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd");
 
     private final RateLimiter rateLimiter;
     private final Retry retry;
@@ -50,6 +61,61 @@ public class PolygonClient {
 
     public List<StockExchangeJSON> getStockExchanges() throws Exception {
         return execute(() -> polygonAPI.getStockExchanges().get(REQUEST_TIMEOUT.toMillis(), TimeUnit.MILLISECONDS));
+    }
+
+    public void outputStockAggregates(final String ticker,
+                                      final LocalDate startDate,
+                                      final LocalDate endDate,
+                                      final Outputter<Aggregate> outputter) throws Exception {
+        Long lastTimestamp = null;
+
+        while (true) {
+            final LocalDate computedStartDate;
+
+            if (lastTimestamp == null) {
+                computedStartDate = startDate;
+            } else {
+                computedStartDate = new LocalDate(lastTimestamp);
+            }
+
+            final var aggregatesList = getStockAggregates(ticker, computedStartDate, endDate);
+
+            for (StockAggregateJSON aggregate: aggregatesList.getResults()) {
+                if (lastTimestamp != null && aggregate.getT() <= lastTimestamp) {
+                    // Skip aggregates we've already received.
+                    continue;
+                }
+
+                outputter.output(aggregate.convert());
+            }
+
+            final var size = aggregatesList.getResults().size();
+            final boolean done = size < AGGREGATES_LIMIT;
+
+            if (done) {
+                break;
+            } else {
+                final var aggregate = aggregatesList.getResults().get(size - 1);
+                lastTimestamp = aggregate.getT();
+            }
+        }
+
+        outputter.finish();
+    }
+
+    private StockAggregateListJSON getStockAggregates(final String ticker,
+                                                      final LocalDate startDate,
+                                                      final LocalDate endDate) throws Exception {
+
+        return execute(() -> polygonAPI.getStockAggregates(
+                ticker,
+                1,
+                AGGREGATES_TIMESPAN,
+                startDate.toString(DATE_TIME_FORMATTER),
+                endDate.toString(DATE_TIME_FORMATTER),
+                false,
+                AGGREGATES_SORT_ASC,
+                AGGREGATES_LIMIT).get(REQUEST_TIMEOUT.toMillis(), TimeUnit.MILLISECONDS));
     }
 
     private <T> T execute(final SupplierWithException<T> request) throws Exception {
--- src/main/java/com/github/ilooner/polygoncli/client/model/StockAggregateJSON.java
+++ src/main/java/com/github/ilooner/polygoncli/client/model/StockAggregateJSON.java
@@ -18,4 +18,19 @@ public class StockAggregateJSON {
     private double vw;
     private long t;
     private long n;
+
+    public Aggregate convert() {
+        final Aggregate aggregate = new Aggregate();
+
+        aggregate.setOpenPrice(o);
+        aggregate.setHighestPrice(h);
+        aggregate.setLowestPrice(l);
+        aggregate.setClosePrice(c);
+        aggregate.setVolume(v);
+        aggregate.setAverage(vw);
+        aggregate.setTimestamp(t);
+        aggregate.setNumberOfItems(n);
+
+        return aggregate;
+    }
 }
