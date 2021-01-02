996d6c2b6361a0ed6f2329f6356b0e5fe50f9057
Timothy Farkas <timothyfarkas@apache.org>
2021-01-01 23:14:48 -0800

    - Make conversions for trades work

--- src/main/java/com/github/ilooner/polygoncli/client/model/json/StockTradeJSON.java
+++ src/main/java/com/github/ilooner/polygoncli/client/model/json/StockTradeJSON.java
@@ -1,24 +1,56 @@
 package com.github.ilooner.polygoncli.client.model.json;
 
+import com.github.ilooner.polygoncli.client.model.Trade;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
-public class StockTradeJSON {
+public class StockTradeJSON implements JSONData<Trade> {
+    private String I = "";
     private long t;
     private long y;
     private long q;
     private String i;
     private int x;
     private long s;
-    private int[] c = new int[0];
+    private List<Integer> c = new ArrayList<>();
     private double p;
     private int z;
+    private int e;
+    private int r;
 
+    @Override
+    public long getTimestampMillis() {
+        // Convert to millis
+        return t / 1000L;
+    }
 
+    @Override
+    public Trade convert() {
+        final var trade = new Trade();
+
+        trade.setOriginalTradeID(I);
+        trade.setExchangeID(x);
+        trade.setPrice(p);
+        trade.setTradeID(i);
+        trade.setCorrectionIndicator(e);
+        trade.setReportingFacilityID(r);
+        trade.setTimestampNano(t);
+        trade.setQuoteTimestampNano(y);
+        trade.setSequenceNo(q);
+        trade.setConditions(c);
+        trade.setSize(s);
+        trade.setTape(z);
+
+        return trade;
+    }
 }
