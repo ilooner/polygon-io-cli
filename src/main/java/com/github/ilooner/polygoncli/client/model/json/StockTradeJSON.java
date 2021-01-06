1a5d6ba9fc031f8e2f92ed0aa1cc0fe59391f895
Timothy Farkas <timothyfarkas@apache.org>
2021-01-05 23:33:36 -0800

    - Trade conditions are not needed.

--- src/main/java/com/github/ilooner/polygoncli/client/model/json/StockTradeJSON.java
+++ src/main/java/com/github/ilooner/polygoncli/client/model/json/StockTradeJSON.java
@@ -52,7 +52,6 @@ public class StockTradeJSON implements JSONData<Trade> {
         trade.setTimestampNano(t);
         trade.setQuoteTimestampNano(y);
         trade.setSequenceNo(q);
-        trade.setConditions(c);
         trade.setSize(s);
         trade.setTape(z);
 
